ef559d149e137f3aa5f4623e39ae1e97710f97e7
Michiel Coulier <michielcoulier@gmail.com>
2020-11-19 11:56:47 +0100

    dashboard shows all agents

--- src/Controller/ManagerController.php
+++ src/Controller/ManagerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,12 +16,28 @@ class ManagerController extends AbstractController
     /**
      * @Route("/dashboard", name="manager", methods={"GET"})
      */
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
         $this->denyAccessUnlessGranted('ROLE_MANAGER');
 
         return $this->render('manager/index.html.twig', [
-            'controller_name' => 'ManagerController',
+            'users' => $userRepository->findByRole(
+                "ROLE_AGENT"
+            )
         ]);
     }
+
+    /**
+     * @Route("/display-agents", name="display_agents", methods={"GET"})
+     */
+    public function showAgents(UserRepository $userRepository)
+    {
+
+        return $this->render('manager/display_agents.html.twig', [
+            'users' => $userRepository->findByRole(
+                "ROLE_AGENT"
+            )
+        ]);
+    }
+
 }
